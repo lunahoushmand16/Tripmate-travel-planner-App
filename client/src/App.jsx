22221f7d2208a54e1ac973fe3c5a7ccecfda15ff
Nina W <nina@Ninas-Air.lan>
2025-05-29 12:39:48 -0700

    added basic code for front end

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      color: 'white',
+      fontSize: '2rem',
+      padding: '4rem',
+      backgroundColor: '#131515',
+      minHeight: '100vh',
+    }}>
+      ✅ TripMate is rendering!
+    </div>
+  );
+}
+
+export default App;
