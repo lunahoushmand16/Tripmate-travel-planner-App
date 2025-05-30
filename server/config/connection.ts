172ea9072cded32546c67f0ba2e7bf55b1a2e3cd
AdrewReyes <andrewreyes.m@gmail.com>
2025-05-29 20:54:30 -0700

    deleted server.ts files/ fixed issues

---
+++ server/config/connection.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tripmate';
+
+mongoose.connect(MONGODB_URI, {
+});
+
+export default mongoose.connection;
